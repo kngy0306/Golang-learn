c156b5f138c9293c79580a57d12d15e6d4cf8bce
kona <kitami.kitano20@gmail.com>
2022-07-04 23:56:49 +0900

    :lipstick: ドライバーを使用してクエリーのロギング出力

--- code_recipe/sql/main.go
+++ code_recipe/sql/main.go
@@ -2,21 +2,53 @@ package main
 
 import (
 	"context"
-	"database/sql"
+	"log"
 
+	"github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
+
+var _ pgx.Logger = (*logger)(nil)
+
+type logger struct{}
+
+func (l *logger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]interface{}) {
+	if msg == "Query" {
+		log.Printf("SQL: %v\nArgs: %v\n", data["sql"], data["args"])
+	}
+}
 
 func main() {
 	ctx := context.Background()
 
-	db, err := sql.Open("pgx", "host=localhost port=5432 user=testuser dbname=testdb password=pass sslmode=disable")
+	config, err := pgx.ParseConfig("user=testuser password=pass host=localhost port=5432 dbname=testdb sslmode=disable")
 	if err != nil {
-		// エラーハンドリング
+		log.Fatalf("parse config: %v\n", err)
+	}
+	config.Logger = &logger{}
+
+	conn, err := pgx.ConnectConfig(ctx, config)
+	if err != nil {
+		log.Fatalf("connect: %v\n", err)
 	}
 
-	err = db.PingContext(ctx)
+	sql := `SELECT schemaname, tablename FROM pg_tables WHERE schemaname = $1;`
+	args := `information_schema`
+
+	rows, err := conn.Query(ctx, sql, args)
 	if err != nil {
-		// エラーハンドリング
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var pgtables []PgTable
+	for rows.Next() {
+		var s, t string
+		if err != rows.Scan(&s, &t); err != nil {
+			// エラー
+		}
+		pgtables = append(pgtables, PgTable{SchemaName: s, TableName: t})
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
